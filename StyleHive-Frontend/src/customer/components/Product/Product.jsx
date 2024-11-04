ccd4dd607a8ec1ce9591dc4f0dbb949a0d947312
AnanthulaSaivyshnav <cs691.team3pace@gmail.com>
2024-11-04 17:22:42 -0500

    pagination changes

--- StyleHive-Frontend/src/customer/components/Product/Product.jsx
+++ StyleHive-Frontend/src/customer/components/Product/Product.jsx
@@ -103,7 +103,7 @@ export default function Product() {
       minDiscount: disccount || 0,
       sort: sortValue || "price_low",
       pageNumber: pageNumber,
-      pageSize: 10,
+      pageSize: 5,
       stock: stock,
     };
     dispatch(findProducts(data));
