e64a9351bf72d293164d3731a197f3afc2e306ef
AnanthulaSaivyshnav <cs691.team3pace@gmail.com>
2024-11-12 23:47:48 -0500

    Cart Item changes

--- StyleHive-Frontend/src/customer/components/Cart/CartItem.jsx
+++ StyleHive-Frontend/src/customer/components/Cart/CartItem.jsx
@@ -23,21 +23,21 @@ const CartItem = ({item}) => {
                 <div className="w-[5rem] h-[5rem] lg:w-[9rem] lg:h-[9rem] ">
                     <img
                         className="w-full h-full object-cover object-top"
-                        src={item.product.imageUrl}
+                        src={item.product?.imageUrl}
                         alt=""
                     />
                 </div>
                 <div className="ml-5 space-y-1">
-                    <p className="font-bold">{item.product.title}</p>
+                    <p className="font-bold">{item.product?.title}</p>
                     <p className="opacity-70">Size:{item.size},White</p>
-                    <p className="opacity-70 mt-2">Seller: {item.product.brand}</p>
+                    <p className="opacity-70 mt-2">Seller: {item.product?.brand}</p>
                     <div className="flex space-x-2 items-center pt-6">
                         <p className="opacity-50 line-through">${item.price}</p>
                         <p className="font-semibold text-lg">
                             ${item.discountedPrice}
                         </p>
                         <p className="text-green-600 font-semibold">
-                        {item.product.discountPersent}% off
+                        {item.product?.discountPersent}% off
                         </p>
                     </div>
                 </div>
